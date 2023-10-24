6ca64abdeb56fc0ef7c85b643dd5e2a489360666
Kim-TaeUk <xodnr0130@naver.com>
2023-10-25 03:48:08 +0900

    [week2] SOPT entity 클래스 : @Embeddable 사용 - 내장 타입으로 정의

--- seminar/src/main/java/com/server/dosopt/seminar/domain/SOPT.java
+++ seminar/src/main/java/com/server/dosopt/seminar/domain/SOPT.java
@@ -1,2 +1,33 @@
-package com.server.dosopt.seminar.domain;public class Sopt {
+package com.server.dosopt.seminar.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.STRING;
+
+/*
+@Embeddable: JPA entity 안의 컬럼을 하나의 객체로 사용하고 싶을 때 사용
+- 값 타입을 정의하는 곳에 표시
+ */
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SOPT {
+    private int generation;
+
+    /*
+    Enumerated 어노테이션 - 2가지 mapping 값 존재
+    1. EnumType.ORDINAL
+    : enum의 순서를 db에 저장
+
+    2. EnumType.STRING
+    : enum의 이름 자체가 db에 저장
+     */
+    @Enumerated(value = STRING)
+    private Part part;
 }
