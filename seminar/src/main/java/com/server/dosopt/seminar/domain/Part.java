66a1845a35eb765d5bd54bdbd4d1d46edf83209f
Kim-TaeUk <xodnr0130@naver.com>
2023-10-25 03:40:51 +0900

    [week2] Part enum 클래스 : @AllArgsConstructor 사용

--- seminar/src/main/java/com/server/dosopt/seminar/domain/Part.java
+++ seminar/src/main/java/com/server/dosopt/seminar/domain/Part.java
@@ -1,2 +1,22 @@
-package com.server.dosopt.seminar.domain;public enum Part {
+package com.server.dosopt.seminar.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/*
+@AllArgsConstructor: 클래스의 모든 field를 포함하는 생성자 생성 - 각 field에 대한 초기화를 처리
+-> 열거형 상수를 초기화하기 간편해짐
+ex. Part server = new Part("server"); -> Part server = Part.SERVER;
+ */
+@Getter
+@AllArgsConstructor
+public enum Part {
+    SERVER("server"),
+    WEB("web"),
+    ANDROID("android"),
+    IOS("ios"),
+    PLAN("plan"),
+    DESIGN("design");
+
+    private final String name;
 }
