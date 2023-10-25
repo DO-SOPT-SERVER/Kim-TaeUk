a7ef5de7f0ee574e290b2d47039cbc86cd23959f
Kim-TaeUk <xodnr0130@naver.com>
2023-10-25 12:22:12 +0900

    [week2] 사용자 생성 비즈니스 로직 구현 MemberService - create

--- seminar/src/main/java/com/server/dosopt/seminar/service/MemberService.java
+++ seminar/src/main/java/com/server/dosopt/seminar/service/MemberService.java
@@ -1,2 +1,43 @@
-package com.server.dosopt.seminar.service;public class MemberService {
+package com.server.dosopt.seminar.service;
+
+import com.server.dosopt.seminar.domain.Member;
+import com.server.dosopt.seminar.dto.MemberCreateRequest;
+import com.server.dosopt.seminar.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+@Service: Service 계층 컴포넌트로 선언,
+SpringApplicationContext에 service bean으로 등록함!
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+    /*
+    @Transactional: Spring에서 선언적 트랜잭션 처리를 지원!
+
+    readOnly = false(default)
+    -> 읽기와 쓰기 모두 수행, 데이터 변경 포함한 작업 수행 시 필요
+
+    readOnly = true
+    -> 읽기만 수행, 데이터 수정하지 않는 경우에 사용
+     */
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @Transactional
+    public String create(MemberCreateRequest request) {
+        Member member = Member.builder()
+                .name(request.name())
+                .nickname(request.nickname())
+                .age(request.age())
+                .sopt(request.sopt())
+                .build();
+
+        Member savedMember = memberJpaRepository.save(member);
+        return savedMember.getId().toString();
+    }
 }
+
