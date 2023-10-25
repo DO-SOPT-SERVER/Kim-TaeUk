8fc6e4e41515a1403e7b4c4f10d569ff96c53324
Kim-TaeUk <xodnr0130@naver.com>
2023-10-25 16:04:20 +0900

    [week2] 특정 사용자 정보 조회 API 비즈니스 로직 구현 MemberService - getMemberByIdV1, V2, V3

--- seminar/src/main/java/com/server/dosopt/seminar/service/MemberService.java
+++ seminar/src/main/java/com/server/dosopt/seminar/service/MemberService.java
@@ -2,7 +2,9 @@ package com.server.dosopt.seminar.service;
 
 import com.server.dosopt.seminar.domain.Member;
 import com.server.dosopt.seminar.dto.MemberCreateRequest;
+import com.server.dosopt.seminar.dto.MemberGetResponse;
 import com.server.dosopt.seminar.repository.MemberJpaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,6 +40,34 @@ public class MemberService {
 
         Member savedMember = memberJpaRepository.save(member);
         return savedMember.getId().toString();
+    }
+
+    public MemberGetResponse getMemberByIdV1(Long id) {
+        /*
+        memberJpaRepository.findById(id)의 반환값은 Optional<Member>임.
+        -> Member 객체가 있을 수도 있고 비어있을 수도 있음
+
+        id에 해당하는 Member 객체가 없는 경우 NoSuchElementException 발생함
+        -> isPresent()나 ifPresent()를 통해 확인 후 호출하고, 비어있을 경우 예외처리하는 것이 올바름
+         */
+        Member member = memberJpaRepository.findById(id).get();
+        return MemberGetResponse.of(member);
+    }
+
+    /*
+    getMemberByIdV2와 getMemberByIdV3는 기능적으로 같음
+    하드코딩한 에러 메시지와 함께 EntityNotFoundException를 직접 던지기 -> getMemberByIdV2
+    findByIdOrThrow 메서드 사용해서 예외 처리 추상화 -> getMemberByIdV3
+     */
+    public MemberGetResponse getMemberByIdV2(Long id) {
+        Member member = memberJpaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+        return MemberGetResponse.of(member);
+    }
+
+    public MemberGetResponse getMemberByIdV3(Long id) {
+        Member member = memberJpaRepository.findByIdOrThrow(id);
+        return MemberGetResponse.of(member);
     }
 }
 
